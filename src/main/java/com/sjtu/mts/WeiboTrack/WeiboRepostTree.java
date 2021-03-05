c089c61979f2b6f124e54fb17d5c72af9cdb03b6
M-More <710599679@qq.com>
2021-03-05 23:17:28 +0800

    finish weibo repost tree class

--- src/main/java/com/sjtu/mts/WeiboTrack/WeiboRepostTree.java
+++ src/main/java/com/sjtu/mts/WeiboTrack/WeiboRepostTree.java
@@ -5,9 +5,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class WeiboRepostTree {
+    private Set<WeiboRepostTree> children = new LinkedHashSet<>(); // LinkedHashSet preserves insertion order
+    private WeiboData data;
+
+    public WeiboRepostTree(WeiboData data) {
+        this.data = data;
+    }
+
+    WeiboRepostTree findChild(WeiboData data) {
+        for (WeiboRepostTree child: children ) {
+            if (child.data.isSameWeibo(data)) {
+                return child;
+            }
+        }
+        return addChild(new WeiboRepostTree(data));
+    }
+
+    WeiboRepostTree addChild(WeiboRepostTree child) {
+        children.add(child);
+        return child;
+    }
 }
