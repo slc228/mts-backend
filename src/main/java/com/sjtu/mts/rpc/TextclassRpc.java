4e8cdf9685db15bc9d60b9367c1cb501700ad60f
rzzhimo <1070634876@qq.com>
2021-04-19 17:54:46 +0800

    文本分类改进

--- src/main/java/com/sjtu/mts/rpc/TextclassRpc.java
+++ src/main/java/com/sjtu/mts/rpc/TextclassRpc.java
@@ -24,10 +24,19 @@ public class TextclassRpc {
         JSONArray json = new JSONArray();
         for (String file : fileContents){
             json.add(file);
-            System.out.println(file);
+            //System.out.println(file);
         }
-        System.out.println(fileContents.size());
-        return  restTemplate.getForObject("http://python-service/predict?textList={1}", String.class,json);
+//        System.out.println(json);
+        HttpHeaders headers = new HttpHeaders();
+        //定义请求参数类型，这里用json所以是MediaType.APPLICATION_JSON
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        //RestTemplate带参传的时候要用HttpEntity<?>对象传递
+        Map<String, Object> requestParam = new HashMap<String, Object>();
+        requestParam.put("textList", json);
+        HttpEntity entity = new HttpEntity(requestParam, headers);
+        //System.out.println(entity);
+        return  restTemplate.postForObject("http://python-service/predict",entity,String.class);
+        //return  restTemplate.getForObject("http://python-service/predict?textList={1}", String.class,json);
     }
     public String clustering(List<String> fileContents){
         JSONArray json = new JSONArray();
@@ -45,7 +54,6 @@ public class TextclassRpc {
         requestParam.put("textList", json);
         HttpEntity entity = new HttpEntity(requestParam, headers);
         //System.out.println(entity);
-        //HttpEntity<Map<String, Object>> request = new HttpEntity<Map<String, Object>>(map, headers);
         return  restTemplate.postForObject("http://clustering-service/kmeans",entity,String.class);
     }
 }
